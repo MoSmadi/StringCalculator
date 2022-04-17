f1812a13430e6cc2150b8930110a5631272480ee
Mohammad Smadi <mo.smadi@outlook.com>
2022-04-17 23:26:09 +0300

    add main files

---
+++ TDD String calculator kata/ICalculator.cs
@@ -0,0 +1,6 @@
+namespace TDD_String_calculator_kata;
+
+public interface ICalculator
+{
+    
+}
---
+++ TDD String calculator kata/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TDD_String_calculator_kata;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void AddEmptyStringShould()
+    {
+        
+    }
+}
